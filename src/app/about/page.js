939477d6cfeffe070c84991ddc8b0e163c05140e
sachinkumarwbd <srpgkumaar1234@gmail.com>
2024-04-07 21:23:39 +0530

    first commit

---
+++ src/app/about/page.js
@@ -0,0 +1,12 @@
+export default function about(){
+    return (
+        <div>
+            <h2 className="text-white text-4xl">about pages</h2>
+        </div>
+    )
+
+    
+
+  
+
+}
---
+++ src/app/profile/page.js
@@ -0,0 +1,10 @@
+
+
+export default function profile() {
+  return (
+    <div>
+      <h1 className="text-white">this is a profile</h1>
+    </div>
+  )
+}
+
